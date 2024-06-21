67eefe4d659e92016c71313f51673c42da2d7713
Ronald G Minnich <rminnich@gmail.com>
2024-06-21 16:22:27 -0700

    put a little color behind peter for fun.
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

--- pjw/draw.go
+++ pjw/draw.go
@@ -13,6 +13,7 @@ import (
 func main() {
 	display := initdisplay.InitDisplay()
 
+	tinydraw.FilledRectangle(display, 0, 0, 256, 256, color.RGBA{R: 255, G: 0, B: 128, A: 255})
 	for _, p := range pixels {
 		tinydraw.FilledRectangle(display, p.x, p.y, 1, 1, color.RGBA{R: p.val, G: p.val, B: p.val, A: 255})
 	}
