fa36e2fa76976a025708bf141ceeec53d4377ae5
Ronald G. Minnich <rminnich@gmail.com>
2024-06-28 23:35:22 -0700

    almost there
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- poly/floodfill.go
+++ poly/floodfill.go
@@ -12,26 +12,27 @@ func floodFill(img *image.Alpha, start image.Point, col color.Alpha) {
 
 	var painting bool
 	r := img.Bounds()
-	col.A = 0
-	for x := r.Min.X; x < r.Max.X; x++ {
-		for y := r.Min.Y; y < r.Max.Y; y++ {
+	for y := r.Min.Y; y < r.Max.Y; y++ {
+		painting = false
+		for x := r.Min.X; x < r.Max.X; x++ {
 			at := img.AlphaAt(x, y).A
-			d("%d,%d:@ %v col %v col.A %v", x, y, at, col, col.A)
+			d("%d,%d=%v col %v col.A %v", y, x, at, col, col.A)
 			if !painting {
 				if at != col.A {
-					d("skip %v,%v", x, y)
+					d("skip %v,%v", y, x)
 					continue
 				}
-				d("start %v,%v", x, y)
+				d("start %v,%v", y, x)
 				painting = true
+				continue
 			} else {
 				if at == col.A {
-					d("stop %v,%v", x, y)
+					d("stop %v,%v", y, x)
 					painting = false
 					continue
 				}
 			}
-			d("paint %v,%v", x, y)
+			d("paint %v,%v", y, x)
 			img.SetAlpha(x, y, col)
 		}
 	}
